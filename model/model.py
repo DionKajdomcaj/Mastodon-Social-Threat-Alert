3ab9be92285b8239fd6f30569e169368341da5a6
Dion Kajdomcaj <dion.kajdomcaj@outlook.com>
2022-04-02 21:40:40 +0200

    fixing flake8 errors

--- model/model.py
+++ model/model.py
@@ -2,7 +2,7 @@
 import pandas as pd
 import pickle
 from sklearn.linear_model import LogisticRegression
-#from sklearn.model_selection import train_test_split
+# from sklearn.model_selection import train_test_split
 
 
 class Model:

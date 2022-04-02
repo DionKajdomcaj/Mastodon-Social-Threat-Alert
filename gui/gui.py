fe35e4ae7a9536d2fad5dd332abfebf808fda0e9
Dion Kajdomcaj <dion.kajdomcaj@outlook.com>
2022-04-02 21:17:08 +0200

    fixing flake8 errors

--- gui/gui.py
+++ gui/gui.py
@@ -25,8 +25,8 @@ class LogIn(tk.Tk):
         window_width = 400
         screen_width = self.winfo_screenwidth()
         screen_height = self.winfo_screenheight()
-        x_cordinate = int((screen_width / 2) - (window_width / 2))
-        y_cordinate = int((screen_height / 2) - (window_height / 2))
+        x = int((screen_width / 2) - (window_width / 2))
+        y = int((screen_height / 2) - (window_height / 2))
         self['bg'] = 'light blue'
 
         self.welcome_label = tk.Label(self, text='Mastodon Threat Alert', 
@@ -90,19 +90,20 @@ class LogIn(tk.Tk):
         self.widgets['running'].add(self.running_end)
 
         self.img = Image.open('gui/images/bg2.png')
-        self.resizable_img = self.img.resize((200,250), Image.ANTIALIAS)
+        self.resizable_img = self.img.resize((200, 250), Image.ANTIALIAS)
         self.new_image = ImageTk.PhotoImage(self.resizable_img)
         self.canvas.pack()
         self.canvas.create_image(0, 0, anchor=tk.NW, image=self.new_image)
         self.canvas.image_types = 'png' 
 
+        text_running = "The program state is\n Currently running..."
         self.running_label1 = tk.Label(self, 
-                                text="The program state is:\n Currently running...",
-                                     font=("Ariel",20), bg='light blue')
+                                        text=text_running, 
+                                        font=("Ariel",20), bg='light blue')
         self.widgets['running'].add(self.running_label1)
 
         self.geometry("{}x{}+{}+{}"
-                    .format(window_width, window_height, x_cordinate, y_cordinate))
+                    .format(window_width, window_height, x, y))
         self.resizable(0,0)
 
     def startApp(self):
@@ -144,20 +145,26 @@ class LogIn(tk.Tk):
                 while time.time() <= t_end:
                     self.update()
 
-
                 if len(self.accounts_reaching_user) > 0:
                     for account in self.accounts_reaching_user:
                         threat_checked_account = self.app.isItThreat(account)
                         account_data = threat_checked_account[0]
                         threat_data = threat_checked_account[1]
                         if threat_data:
-                            showinfo(message = "You have a threat!\n Go back to take actions !")
+                            message = 'You have a threat!\n Go back to take actions !'
+                            showinfo(message=message)
+
                             self.done = False
                             self.string_var = tk.StringVar()
-                            self.action_combobox = Combobox(self, textvariable = self.string_var)
-                            self.action_combobox['values'] = ("Nothing", "Block", "Report")
+                            self.action_combobox = Combobox(self, 
+                                                        textvariable=self.string_var)
+                            self.action_combobox['values'] = ("Trust", "Block", "Report")
+
                             self.widgets['action'].add(self.action_combobox)
-                            self.action_button = tk.Button(self, text = 'Take action', command = lambda : self.takeAction(account_data,threat_data))
+
+                            self.action_button = tk.Button(self, text='Take action', 
+                            command=lambda : self.takeAction(account_data,threat_data))
+
                             self.widgets['action'].add(self.action_button)
 
                             self.cleanRunning()

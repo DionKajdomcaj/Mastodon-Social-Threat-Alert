e765e22c64accf6ff3e23ffd21588ea9f66ce9d6
Dion Kajdomcaj <dion.kajdomcaj@outlook.com>
2022-04-07 22:00:43 +0200

    Fixing flake8 errors

--- test_calc.py
+++ test_calc.py
@@ -1,10 +1,12 @@
 import calc
 
-#print(calculator.add(2, 2))
+
 class TestCalculator:
     def test_addition(self):
         assert 4 == calc.add(2, 2)
+
     def test_subtraction(self):
         assert 2 == calc.subtract(4, 2)
+        
     def test_multiplication(self):
         assert 100 == calc.multiply(10, 10)
